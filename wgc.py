12869f4e5b70a95d4290511995c361516610993d
Giuseppe Cimo <gpikkio@gmail.com>
2022-06-14 10:33:36 +0200

    Proper files are now created with the right names.

--- wgc.py
+++ wgc.py
@@ -3,7 +3,6 @@ def webgeocalc(sc, ut_start, ut_end, step, obs, ref, timesystem):
     from webgeocalc import StateVector
     
     vectors = StateVector(kernel_paths=['pds/data/mex-e_m-spice-6-v2.0/mexsp_2000/EXTRAS/MK/MEX_V03.TM'],
-                          #times = ut_start,
                           intervals=[ut_start, ut_end],
                           time_step = step,
                           time_step_units='SECONDS',
@@ -12,11 +11,10 @@ def webgeocalc(sc, ut_start, ut_end, step, obs, ref, timesystem):
                           time_system = timesystem,
                           reference_frame=ref,
                           aberration_correction='NONE'
-                          #state_representation='RECTANGULAR')
     )
-    return vectors.run()    
+    return vectors.run()
 
-def inputs(frame):
+def inputs(sc, frame):
     params = []
     obs= 'EARTH'
     ref = 'J2000'
@@ -31,9 +29,10 @@ def inputs(frame):
     utstart = '2018-06-04 02:37:10'
     utend   = '2018-06-04 02:37:20'
     steps=1
-    
-    params = [utstart, utend, steps, obs, ref, ts]
+
+    params = [sc, utstart, utend, steps, obs, ref, ts]
     return params
+
 
 if __name__ == '__main__':
 
@@ -43,15 +42,17 @@ if __name__ == '__main__':
 
     sc = 'MEX'
     rs = ['bcrs', 'gcrs', 'gtrs']
+
+    for frames in range(len(rs)):
+        filename = sc.lower()+'.'+rs[frames]+'.'+inputs(sc,rs[frames])[-1].lower()+'.'\
+            +inputs(sc,rs[frames])[1][2:4]+inputs(sc,rs[frames])[1][5:7]+inputs(sc,rs[frames])[1][8:10]+'.eph'
     
-    stateVectors=pd.DataFrame(webgeocalc(sc, *inputs(rs[2])))#utstart,utend,steps,sc,obs,ref,ts))
-    vects = stateVectors[['DATE','X','Y','Z','D_X_DT','D_Y_DT','D_Z_DT']]
-    np.savetxt(r'tmp.txt', vects.values, fmt='%s')
-        
-    date_re = re.compile('(\d{4})-(\d{2})-(\d{2}).(\d{2})\:(\d{2})\:(\d{2}.\d{3}).*(UTC|TDB)(.*)')
-    for lines in open('tmp.txt'):
-        match_date = date_re.match(lines)
-        if match_date:
-            print(match_date.group(1), match_date.group(2), match_date.group(3), match_date.group(4),
-                  match_date.group(5),match_date.group(6), match_date.group(8).format())
+        stateVectors=pd.DataFrame(webgeocalc(*inputs(sc,rs[frames])))
+        vects = stateVectors[['DATE','X','Y','Z','D_X_DT','D_Y_DT','D_Z_DT']]
             
+        #date_re = re.compile('(\d{4})-(\d{2})-(\d{2}).(\d{2})\:(\d{2})\:(\d{2}.\d{3}).*(UTC|TDB)(.*)')
+        date_re = re.compile('(.*).*(UTC|TDB)(.*)')
+        vects['DATE'] = vects['DATE'].str.extract(date_re)[0]
+        np.savetxt(r'tmp.txt', vects.values, fmt='%s')
+        with open(filename, 'w') as f:
+            np.savetxt(filename, vects.values, fmt='%s')
