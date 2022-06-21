f3128ff1d4163598d98dfe2f79fcc535acf48f53
gpikkio <gpikkio@gmail.com>
2022-06-21 14:20:27 +0200

    Selection of API (ESA or NASA) based on the spacecraft name. It searches on the API.url for the proper kernels. If the proper S/C name is used, the program should find the righ kernels to load and peform the right calculations.

--- wgc.py
+++ wgc.py
@@ -1,8 +1,23 @@
 def webgeocalc(sc, ut_start, ut_end, step, obs, ref, timesystem):
 
-    from webgeocalc import StateVector
+    import importlib
     
-    vectors = StateVector(kernels=[findKernels(sc),1,2],
+    kernel_sc, apis = findKernels(sc)[0], findKernels(sc)[1]
+                
+    if apis == 'ESA_API':
+        from webgeocalc import ESA_API as API
+        from webgeocalc import StateVector
+        kernel_solar = 3
+        kernel_leap = 4
+    else:
+        from webgeocalc import API
+        from webgeocalc import StateVector
+        kernel_solar = 1
+        kernel_leap = 2
+
+    ap=API.url
+    print(ap)
+    vectors = StateVector(api=ap,kernels=[kernel_sc,kernel_solar,kernel_leap],
                           intervals=[ut_start, ut_end],
                           time_step = step,
                           time_step_units='SECONDS',
@@ -14,18 +29,37 @@ def webgeocalc(sc, ut_start, ut_end, step, obs, ref, timesystem):
     ) 
     return vectors.run()
 
+
 def findKernels(sc):
     import json
     import urllib.request
+    from webgeocalc import ESA_API
     from webgeocalc import API
 
-    link = API.url+'/kernel-sets'
-    with urllib.request.urlopen(link) as url:
+    if sc.lower() == 'tgo':
+        sc = 'em16'
+    s = sc+'_ops'
+    link_esa = ESA_API.url+'/kernel-sets'
+    link_nasa = API.url+'/kernel-sets'
+    kernelSet = ''
+    api = ''
+    with urllib.request.urlopen(link_esa) as url:
         kernelSets = json.loads(url.read().decode())
         for i in range(len(kernelSets['items'])):
-            if sc.lower() in kernelSets['items'][i]['missionId'].lower() or sc.lower() in kernelSets['items'][i]['caption'].lower():
-                kernelSet = kernelSets['items'][i]['kernelSetId']
-    return int(kernelSet)
+            if sc.lower() in kernelSets['items'][i]['missionId'].lower():
+                api = 'ESA_API'
+                if s.lower() in kernelSets['items'][i]['missionId'].lower() or s.lower() in kernelSets['items'][i]['caption'].lower():
+                    kernelSet = kernelSets['items'][i]['kernelSetId']
+        if api == '':
+            api = 'NASA_API'
+            with urllib.request.urlopen(link_nasa) as url:
+                kernelSets = json.loads(url.read().decode())
+                for i in range(len(kernelSets['items'])):
+                    if sc.lower() in kernelSets['items'][i]['missionId'].lower() or sc.lower() in kernelSets['items'][i]['caption'].lower():
+                        kernelSet = kernelSets['items'][i]['kernelSetId']
+                             
+    return int(kernelSet), api
+
 
 def inputs(sc, frame):
     params = []
@@ -39,12 +73,13 @@ def inputs(sc, frame):
     elif frame == 'gtrs':
         ref = 'EARTH_FIXED'
                 
-    utstart = '2018-06-04 02:37:10'
-    utend   = '2018-06-04 02:37:20'
+    utstart = '2020-06-04 02:37:10'
+    utend   = '2020-06-04 02:37:20'
     steps=1
 
     params = [sc, utstart, utend, steps, obs, ref, ts]
     return params
+
 
 
 if __name__ == '__main__':
@@ -53,8 +88,8 @@ if __name__ == '__main__':
     import numpy as np
     import re
 
-    sc = 'MRO'
-    rs = ['bcrs', 'gcrs', 'gtrs']
+    sc = 'mex'
+    rs = ['bcrs']#, 'gcrs', 'gtrs']
 
     for frames in range(len(rs)):
         filename = sc.lower()+'.'+rs[frames]+'.'+inputs(sc,rs[frames])[-1].lower()+'.'\
